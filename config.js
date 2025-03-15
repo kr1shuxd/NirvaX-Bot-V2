2783bdf9936c01e17964b8578c1e00da11833e71
kr1shuxd <daddykrishu@gmail.com>
2025-03-15 05:11:52 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1282344443109838903", ""], 
+  ownerID: ["1282344443109838903", 1035523468650872883"], 
   mongodbUri : "",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
+  embedColor: "#ff0000",
   activityName: "NirvaX | /help", // This is bot status Write Anything here 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://dsc.gg/nirvax",
